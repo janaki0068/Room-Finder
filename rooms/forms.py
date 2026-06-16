d890470c4742031c1c87abcd8ea81d2e71c7b97d
anushka155 <annggrg@gmail.com>
2026-06-16 14:08:22 +0545

    register

---
+++ rooms/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import User
+
+class RegisterForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    phone_number = forms.CharField(max_length=10)
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
+        
+
+        return cleaned_data
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import RegisterForm
 
 # Create your views here.
+
+# REGISTER
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+
+            if User.objects.filter(email=form.cleaned_data['email']).exists():
+                messages.error(request, 'Email already exists.')
+                return render(request, 'register.html', {'form': form})
+            
+            user = User.objects.create_user(
+                username=form.cleaned_data['email'],
+                email=form.cleaned_data['email'],
+                first_name=form.cleaned_data['first_name'],
+                last_name=form.cleaned_data['last_name'],
+                password=form.cleaned_data['password']
+            )
+
+            messages.success(request, 'User registered successfully.')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+                   
+    return render(request, 'register.html',{'form': form})
