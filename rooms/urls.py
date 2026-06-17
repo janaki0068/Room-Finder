5e6873dff62796e0c15f99d7ac2f5224174c8aa2
anushka155 <annggrg@gmail.com>
2026-06-17 13:14:05 +0545

    add login required and auth popup for saved/list/profile

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,7 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name="login"),
-    path('register/', views.register, name='register'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 
     path('search/',views.search_rooms, name='search_rooms'),
+
+    path('saved/', views.saved_view, name='saved_view'),
+    path('list/', views.list_view, name='list_view'),
+    path('profile/', views.profile_view, name='profile_view'),
 ]
