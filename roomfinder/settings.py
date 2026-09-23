320cd4ff8fad3952cde4bfb4705ab103d44fd29b
Janaki Rai <raijanaki68@gmail.com>
2026-09-23 08:07:40 +0545

    Add Render domain to ALLOWED_HOSTS

--- roomfinder/settings.py
+++ roomfinder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z)hnw9-4s+tc*ayeeniwmx(7jrii1()=)7of%8=y&vct9h(8l0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['room-finder-mjni.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
